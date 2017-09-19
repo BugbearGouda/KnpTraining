17f65debfe980baa1109b795751c3a3da0abbc17
alan foley <alan@metaregistrar.com>
2017-09-19 08:58:23 +0200

    Security video (first 30%)

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -1,10 +1,26 @@
 <?php
 namespace AppBundle\Entity;
 
+use Symfony\Component\Security\Core\Role\Role;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Doctrine\ORM\Mapping as ORM;
 
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="user")
+ */
 class User implements UserInterface
 {
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+    /**
+     * @ORM\Column(type="string", unique=true)
+     */
     private $email;
 
     public function getUsername()
@@ -14,22 +30,19 @@ class User implements UserInterface
 
     public function getRoles()
     {
-        // TODO: Implement getRoles() method.
+        return ['ROLE_USER'];
     }
 
     public function getPassword()
     {
-        // TODO: Implement getPassword() method.
     }
 
     public function getSalt()
     {
-        // TODO: Implement getSalt() method.
     }
 
     public function eraseCredentials()
     {
-        // TODO: Implement eraseCredentials() method.
     }
 
     /**
